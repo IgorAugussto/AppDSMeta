d417910a1897e4441e75da4d81cbd7e38bcbc476
Igor Augusto <igor-augustofc@hotmail.com>
2022-11-18 02:54:48 -0300

    Others components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,17 @@
-import NotificationButton from "./assets/components/notificationButton"
+import Header from "./assets/components/Header"
+import NotificationButton from "./assets/components/NotificationButton"
+import SalesCard from "./assets/components/SalesCard"
 
 function App() {
   return (
     <>
-      <h1>Olá mundo !</h1>
-      <NotificationButton />
-      <NotificationButton />
+      <Header />
+      <main>
+        <section id="sales" />
+          <div className="dsmeta-container">
+            <SalesCard />
+          </div>
+      </main>
     </>
   )
 }
